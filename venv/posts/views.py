6a9b3ed0920c960c0112247ea4c6acee705550ec
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-16 05:32:55 -0300

    Comecei a configurar os templates

--- venv/posts/views.py
+++ venv/posts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from .models import Post
 
 # Create your views here.
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts\index.html'
 
 class PostBusca(PostIndex):
     pass
