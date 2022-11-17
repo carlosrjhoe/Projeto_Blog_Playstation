d5b16442e97524e2611adb4f925a6f4861001239
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-16 21:19:12 -0300

    Melhorando a aplicação dos templates

--- venv/posts/views.py
+++ venv/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 # Create your views here.
 
@@ -11,13 +12,26 @@ class PostIndex(ListView):
   template_name = 'posts\index.html'
   context_object_name = 'posts'
 
+  def get_queryset(self):
+    """Ordenar os post de forma invertida. Sempre o ultimo post será mostrado em 1º"""
+    """(Q, Count, Case, When) Serve para contar a quantidade de comentarios"""
+    qs = super().get_queryset()
+    qs = qs.order_by('-id').filter(publicado_post=True)
+    qs = qs.annotate(
+      numero_comentarios = Count(
+        Case(
+          When(comentario__publicado_comentario=True, then=1)
+        )
+      )
+    )
+    return qs
 
 class PostBusca(PostIndex):
   pass
 
 
 class PostCategoria(PostIndex):
-  pass
+  template_name = 'posts\post_categoria.html'
 
 
 class PostDetalhes(UpdateView):
