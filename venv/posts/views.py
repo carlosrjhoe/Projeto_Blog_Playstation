d593e879b595c13e0c1be3aa993d3554b1c543c8
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-18 05:14:25 -0300

    Implementando a funcionalidade de PostBusca

--- venv/posts/views.py
+++ venv/posts/views.py
@@ -8,31 +8,59 @@ from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
-  model = Post
-  template_name = 'posts\index.html'
-  context_object_name = 'posts'
+    model = Post
+    template_name = 'posts\index.html'
+    context_object_name = 'posts'
 
-  def get_queryset(self):
-    """Ordenar os post de forma invertida. Sempre o ultimo post será mostrado em 1º"""
-    """(Q, Count, Case, When) Serve para contar a quantidade de comentarios"""
-    qs = super().get_queryset()
-    qs = qs.order_by('-id').filter(publicado_post=True)
-    qs = qs.annotate(
-      numero_comentarios = Count(
-        Case(
-          When(comentario__publicado_comentario=True, then=1)
+    def get_queryset(self):
+        """Ordenar os post de forma invertida. Sempre o ultimo post será mostrado em 1º"""
+        """(Q, Count, Case, When) Serve para contar a quantidade de comentarios"""
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
         )
-      )
-    )
-    return qs
+        return qs
+
 
 class PostBusca(PostIndex):
-  pass
+    template_name = 'posts\post_busca.html'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+        
+        if not termo:
+            return qs
+        
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+        
+        return qs
+    
 
 class PostCategoria(PostIndex):
-  template_name = 'posts\post_categoria.html'
+    template_name = 'posts\post_categoria.html'
+
+    def get_queryset(self):
+        """Filtrando por categoria"""
+        qs = super().get_queryset()
+        categoria = self.kwargs.get('categoria', None)
+        if not categoria:
+            return qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+        return qs
 
 
 class PostDetalhes(UpdateView):
-  pass
+    pass
