e8d1414a8114c435ed23c7a06b500a3bb0247cca
Carlos Roberto <carlosrjhoe@gmail.com>
2022-11-19 04:59:50 -0300

    Implementando os comentários dos post e se o usuário está logado

--- venv/posts/views.py
+++ venv/posts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
 from django.db.models import Q, Count, Case, When
 from comentarios.forms import FormComentario
+from comentarios.models import Comentario
+from django.contrib import messages
 
 # Create your views here.
 
@@ -70,3 +72,16 @@ class PostDetalhes(UpdateView):
     model = Post
     form_class = FormComentario
     context_object_name = 'post'
+    
+    def form_invalid(self, form):
+        post = self.get_object()
+        comentario = Comentario(**form.cleaned_data)
+        comentario.post_comentario = post
+        
+        if self.request.user.is_authenticated:
+            """Verificação se login do usuário"""
+            comentario.usuario_comentario = self.request.user
+
+        comentario.save()
+        messages.success(self.request, 'Comentário enviado com sucesso!')
+        return redirect('post_detalhes', pk=post.id)
